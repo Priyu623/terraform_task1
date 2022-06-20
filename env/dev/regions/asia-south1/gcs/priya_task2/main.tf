5543ea727f59ce9bb7b926650745723e1df25718
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2022-06-20 13:46:35 +0530

    Create main.tf

---
+++ env/dev/regions/asia-south1/gcs/priya_task2/main.tf
@@ -0,0 +1,20 @@
+module "gcs-bucket" {
+  source             = "../../../../../../modules/gcs_bucket"
+  count   = length(var.buckets)
+  name               = var.buckets[count.index]["name"]
+  project_id         = var.project_id
+  location           = var.buckets[count.index]["location"]
+  storage_class      = var.buckets[count.index]["storage_class"]
+  versioning         = var.buckets[count.index]["versioning"]
+  labels             = var.buckets[count.index]["labels"]
+  force_destroy      = false
+  iam_members        = var.buckets[count.index]["iam_members"]
+  lifecycle_rules    = var.buckets[count.index]["lifecycle_rules"]
+  bucket_policy_only = var.buckets[count.index]["bucket_policy_only"]
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = module.gcs-bucket[1].bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
